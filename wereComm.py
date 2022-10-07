73668a5cd571474154f90c98d663973c5c863f05
noname201012345 <58730410+noname201012345@users.noreply.github.com>
2022-10-08 01:32:49 +0700

    Update wereComm.py

--- wereComm.py
+++ wereComm.py
@@ -16,11 +16,6 @@ header = {"Authorization": "Bearer {}".format(rtoken)}
 link="https://api.github.com/repos/noname201012345/PartnerBot/contents/"
     
 def wereComm(client:discord.Client):
-    @client.command()
-    async def help(ctx):
-        helpEmbed = discord.Embed(title="Các lệnh của Bot:",color=0x00FFF3,description=f"**Prefix:** {client.command_prefix}\n\n> **Lệnh MultiChat:**\n\n• **msview** - Xem các phòng chat liên server\n• **join** __*tên phòng chat*__ - Tham gia một phòng chat\n• **leave** - Rời phòng chat hiện tại\n• **msban** __*id*__ - Ban người có id trong câu lệnh khỏi phòng chat\n• **create** __*tên phòng chat*__ - Tạo phòng chat liên server mới\n\n> **Lệnh Ma Sói:**\n\n• **wwcreate** - Tạo danh mục ma sói (bắt buộc phải tạo nếu muốn chơi ma sói)\n• **werewolf** __*tên phòng*__ - Tạo phòng ma sói\n• **joinww** __*tên phòng*__ - Tham gia phòng ma sói\n• **delww** - Xóa phòng ma sói bạn là chủ phòng\n• **leadww** __*người chơi*__ - Chuyển chủ phòng cho người khác\n• **leaveww** - Rời khỏi phòng ma sói\n• **roomww** - Kiểm tra các phòng ma sói hiện có\n• **setupww** - Setup phòng ma sói của bạn\n• **wwstart** - Bắt đầu game ma sói")
-        helpEmbed.set_author(name="Gamer Hệ Đần",icon_url=client.get_guild(1010936069329522731).icon.url,url="https://discord.gg/gamerhedan")
-        await ctx.send(embed=helpEmbed)
   
     @client.command(aliases=["ms","ww"])
     async def werewolf(ctx, name):
