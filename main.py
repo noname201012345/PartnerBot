b6eac5cd3d0c114872182e8af6d8d8cff27800f3
noname201012345 <58730410+noname201012345@users.noreply.github.com>
2022-10-08 01:32:22 +0700

    Update main.py

--- main.py
+++ main.py
@@ -27,27 +27,6 @@ link="https://api.github.com/repos/noname201012345/PartnerBot/contents/"
 async def on_ready():
     print(f'Successfully logged in as {client.user}')
 
-@client.command()
-async def del_web(ctx):
-    with open("data.json", "r") as f:
-        data = json.load(f)
-    with open("multichat.json", "r") as f:
-        mchat = json.load(f)
-    guild = str(ctx.guild.id)
-    if guild in data:
-        room = data[guild]["id"]
-        partner = mchat[room]
-        for x in partner:
-            try:
-                wid = data[x]["webhook"]
-                guild = client.get_guild(int(x))
-                channel = client.get_channel(data[x]["channel"])
-                webhook = await client.fetch_webhook(wid)
-                await webhook.delete()
-                await ctx.send(f"deleted webhook from Guild: {guild.name}")
-            except:
-                pass
-
 wereComm(client)
 MultiChat(client)
                                 
