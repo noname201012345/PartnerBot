309372c8511cbe64c6e72ad0f29a9b455accaa55
noname201012345 <58730410+noname201012345@users.noreply.github.com>
2022-11-14 19:13:35 +0700

    Update main.py

--- main.py
+++ main.py
@@ -9,6 +9,7 @@ import math
 import asyncio
 from wereComm import wereComm
 from MultiChat import MultiChat
+from team import Team
 
 load_dotenv()
 
@@ -25,7 +26,7 @@ link="https://api.github.com/repos/noname201012345/PartnerBot/contents/"
 
 @client.command()
 async def help(ctx):
-    helpEmbed = discord.Embed(title="Các lệnh của Bot:",color=0x00FFF3,description=f"**Prefix:** {client.command_prefix}\n\n> **Lệnh MultiChat:**\n\n• **msview** - Xem các phòng chat liên server\n• **join** __*tên phòng chat*__ - Tham gia một phòng chat\n• **leave** - Rời phòng chat hiện tại\n• **msban** __*id*__ - Ban người có id trong câu lệnh khỏi phòng chat\n• **mod** __**Tag Member**__- Cấp quyền mod cho người dùng\n• **create** __*tên phòng chat*__ - Tạo phòng chat liên server mới\n\n> **Lệnh Ma Sói:**\n\n• **wwcreate** - Tạo danh mục ma sói (bắt buộc phải tạo nếu muốn chơi ma sói)\n• **werewolf** __*tên phòng*__ - Tạo phòng ma sói\n• **joinww** __*tên phòng*__ - Tham gia phòng ma sói\n• **delww** - Xóa phòng ma sói bạn là chủ phòng\n• **leadww** __*người chơi*__ - Chuyển chủ phòng cho người khác\n• **leaveww** - Rời khỏi phòng ma sói\n• **roomww** - Kiểm tra các phòng ma sói hiện có\n• **setupww** - Setup phòng ma sói của bạn\n• **wwstart** - Bắt đầu game ma sói\n\n> **Lệnh Khác:**\n\n• **avatar** - Show avatar của bạn hoặc người khác\n• **rolega** - Nhận role giveaway để tạo giveaway")
+    helpEmbed = discord.Embed(title="Các lệnh của Bot:",color=0x00FFF3,description=f"**Prefix:** {client.command_prefix}\n\n> **Lệnh MultiChat:**\n\n• **msview** - Xem các phòng chat liên server\n• **join** __*tên phòng chat*__ - Tham gia một phòng chat\n• **leave** - Rời phòng chat hiện tại\n• **msban** __*id*__ - Ban người có id trong câu lệnh khỏi phòng chat\n• **mod** __**Tag Member**__- Cấp quyền mod cho người dùng\n• **create** __*tên phòng chat*__ - Tạo phòng chat liên server mới\n\n> **Lệnh Ma Sói:**\n\n• **wwcreate** - Tạo danh mục ma sói (bắt buộc phải tạo nếu muốn chơi ma sói)\n• **werewolf** __*tên phòng*__ - Tạo phòng ma sói\n• **joinww** __*tên phòng*__ - Tham gia phòng ma sói\n• **delww** - Xóa phòng ma sói bạn là chủ phòng\n• **leadww** __*người chơi*__ - Chuyển chủ phòng cho người khác\n• **leaveww** - Rời khỏi phòng ma sói\n• **roomww** - Kiểm tra các phòng ma sói hiện có\n• **setupww** - Setup phòng ma sói của bạn\n• **wwstart** - Bắt đầu game ma sói\n\n> **Lệnh Khác:**\n\n• **avatar** - Show avatar của bạn hoặc người khác\n• **rolega** - Nhận role giveaway để tạo giveaway\n• **CreateTeam** *Chủ Nhóm* *Tên Team* *Màu Role* - Tạo một nhóm hoạt động mới\n **AddMem** *Member* - Thêm người vào nhóm bạn là trưởng nhóm")
     helpEmbed.set_author(name="Gamer Hệ Đần",icon_url=client.get_guild(1010936069329522731).icon.url,url="https://discord.gg/gamerhedan")
     await ctx.send(embed=helpEmbed)
 
@@ -53,6 +54,7 @@ async def rolega(ctx):
     
 wereComm(client)
 MultiChat(client)
+Team(client)
                                 
 token = os.getenv("token")
 client.run(token)
